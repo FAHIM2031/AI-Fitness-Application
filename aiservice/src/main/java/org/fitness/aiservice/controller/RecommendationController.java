52b3db7226f9d873664262c964fbeca9829359b5
FAHIM2031 <khaleduzzamanfahim@gmail.com>
2026-09-01 13:10:41 +0600

    Building AI Service with OpenAI / Generative AI

--- aiservice/src/main/java/org/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/org/fitness/aiservice/controller/RecommendationController.java
@@ -1,13 +1,32 @@
 package org.fitness.aiservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.fitness.aiservice.model.Recommendation;
+import org.fitness.aiservice.service.RecommendationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/")
+@RequestMapping("/api/recommendations")
 public class RecommendationController {
 
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId) {
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
+    }
+
+
+    @GetMapping("/activity/{activityId}")
+    public ResponseEntity<Recommendation>getActivityRecommendation(@PathVariable String activityId) {
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
 
 }
