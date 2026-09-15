c463840701b719efa999b707f87adbc53032ad41
FAHIM2031 <khaleduzzamanfahim@gmail.com>
2026-09-15 15:28:13 +0600

    Processing Recommendations End-to-End

--- aiservice/src/main/java/org/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/org/fitness/aiservice/service/ActivityMessageListener.java
@@ -3,6 +3,8 @@ package org.fitness.aiservice.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.fitness.aiservice.model.Activity;
+import org.fitness.aiservice.model.Recommendation;
+import org.fitness.aiservice.repository.RecommendationRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,15 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private  final ActivityAIService activityAIService;
+    private  final RecommendationRepository recommendationRepository;
+
     @KafkaListener(topics="${kafka.topic.name}",groupId = "activity-processor-group")
     public void processActivity(Activity activity) {
         log.info("Received Activity for processing:{}",activity.getUserID());
-        activityAIService.generateRecommendation(activity);
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
+
+
     }
 
 }
